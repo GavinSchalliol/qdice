3cfdce122990035c21d96c007f8faf95b4727a0f
GavinSchalliol <gschalli@thoughtworks.com>
2016-02-21 19:21:37 +0100

    list of 10 words to check now displays correctly, with indices

--- wordcheck.py
+++ wordcheck.py
@@ -5,13 +5,13 @@ from collections import deque
 from config import inputFile, checkedFile, uncheckedFile
 
 prompt = ">>"
-toCheck = []
+uncheckedLines = []
 
 def checkEnvironment():
-	if os.path.isfile(inputFile) == false:
+	if os.path.isfile(inputFile) == 0:
 		print "ERROR: can't find input file. Check to make sure it exists and is correctly named in `config.py`"
 		sys.exit(1)
-	if os.path.isfile(uncheckedFile) == false:
+	if os.path.isfile(uncheckedFile) == 0:
 		shutil.copyfile(inputFile, uncheckedFile)
 	print "Performing sanity checks...done"
 
@@ -19,16 +19,16 @@ def getUncheckedLines():
 	f1 = open(uncheckedFile, 'r')
 	uncheckedLines = f1.readlines()
 	f1.close()
+	return uncheckedLines
 
 # Currently, this just takes the first ten entries from the uncheckedLines list, moves them to a buffer, prints them, and returns them. More to come!
 def main():
-	toCheck[0:9] = uncheckedLines[0:9]
-	queue = deque(uncheckedLines)
-	del uncheckedLines[0:9]
-	print toCheck
+	toCheck = []
+	uncheckedLines = getUncheckedLines()
+	toCheck[0:10] = uncheckedLines[0:10]
+	toCheck = map(lambda s: s.strip(), toCheck)
 	for i in toCheck:
-		queue.appendleft(i)
+		print "[" + str(toCheck.index(i)) + "] " + i
 
 checkEnvironment()
-getUncheckedLines()
 main()
